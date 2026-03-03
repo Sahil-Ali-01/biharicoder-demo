532d144120f8e33d77a6124118ef4e2a4708543b
Sahil Ali <sahilcoder189@gmail.com>
2026-03-03 22:14:40 +0530

    Integrate Simple Analytics in RootLayout
    Added Simple Analytics script to RootLayout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
+import Script from 'next/script'
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
@@ -39,6 +40,15 @@ export default function RootLayout({
         {children}
         <Analytics />
       </body>
+    </html>
+  )
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js"  />
     </html>
   )
 }
